e9607299f0d1d017e00329846c6595db82001990
mariapilan <dudapilan06@hotmail.com>
2024-04-02 15:46:55 -0300

    atualizando

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -21,4 +21,18 @@ class ProdutoController extends Controller
         });
         return response()->json($produtosComImagem);
     }
+
+    public function store(Request $request){
+        $produtoData = $request->all();
+
+    if($request->hasFile('imagem')){
+        $imagem = $request->file('imagem');
+        $nomeImagem = time().'.'.$imagem->getClientOriginalExtension();
+        $caminhoImagem = $imagem->storeAs('imagens/produtos', $nomeImagem, 'public');
+        $produtoData['imagem'] = $caminhoImagem;
+    }
+    $produto = Produto::create($produtoData);
+    return response()->json(['produto'=>$produto], 201); 
+    }
+
 }
